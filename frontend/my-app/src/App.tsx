b2358b8a90d83617874b2ddb70d397a126be2c72
Abril-Oli <abril.olivera2003@gmail.com>
2025-05-09 01:41:56 -0300

    Estructura mejorada de carpetas, dependencias package y maquetacion simple de login

---
+++ frontend/my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Login from "./features/auth/auth/Login";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="bg-blue-500 text-white p-4 text-center">
+        Maquetado Login Mutual 17 de Junio
+      </div>
+
+      <Login />
+    </div>
+  );
+}
+export default App;
