5fed77e9e01b2d1421da9df3c8eaa22ed6458ecb
aribul <bulacio.ariela@gmail.com>
2025-06-01 20:30:50 -0300

    se agregó boton de prueba. (falta hacer una página mas "home" del módulo)

--- frontend/Mutual/src/pages/dashboard/Dashboard.tsx
+++ frontend/Mutual/src/pages/dashboard/Dashboard.tsx
@@ -52,9 +52,15 @@ const Dashboard: React.FC<DashboardProps> = ({ hasNotifications = true }) => {
                 </h3>
                 <button
                   onClick={() => navigate("/proveedores/ordenes/nueva")}
-                  className="mt-2 px-8 py-3 bg-blue-600 text-white rounded-full text-lg font-semibold hover:bg-blue-700 transition"
+                  className="mt-2 px-4 py-3 bg-blue-600 text-white rounded-full text-lg font-semibold hover:bg-blue-700 transition"
                 >
-                  Ingresar
+                  Proveedores
+                </button>
+                <button
+                  onClick={() => navigate("/proveedores/ordenes/nueva")}
+                  className="mt-2 px-4 py-3 bg-blue-600 text-white rounded-full text-lg font-semibold hover:bg-blue-700 transition"
+                >
+                  Ordenes de Compra
                 </button>
               </div>
             </div>
