84237dad18461a49df4cdaebd5c6b1ed66c94440
aribul <bulacio.ariela@gmail.com>
2025-06-16 19:55:04 -0300

    modifico ruta del sidebar

--- frontend/Mutual/src/pages/modules/suppliers/Suppliers.tsx
+++ frontend/Mutual/src/pages/modules/suppliers/Suppliers.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../dashboard/components/Header';
-import CreateOrder from './createOrder';
+import CreateOrder from './createService';
 
 const SuppliersSidebar: React.FC<{ navigate: (path: string) => void }> = ({ navigate }) => (
   <nav className="h-full flex flex-col p-4 bg-white">
