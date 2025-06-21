7d72392c49cc2c5548c04da66675c70d8f6bc5b3
Abril-Oli <abril.olivera2003@gmail.com>
2025-06-21 02:07:47 -0300

    Se agrego funcionalidad crear nuevo prestamo + estilo de botones

--- frontend/Mutual/src/pages/modules/loans/listLoans.tsx
+++ frontend/Mutual/src/pages/modules/loans/listLoans.tsx
@@ -5,7 +5,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import Sidebar from "../../dashboard/components/Sidebar";
 import Modal from "../../../components/modal";
 
-const initialLoans = [
+export const initialLoans = [
   {
     id: 1,
     associateDni: "30123456",
@@ -306,12 +306,28 @@ const Loans: React.FC<DashboardProps> = ({
                   </select>
                 </div>
 
-                <button
-                  onClick={() => navigate("/prestamos/crear")}
-                  className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-2 rounded font-semibold shadow transition w-full md:w-auto"
-                >
-                  Solicitar Préstamo
-                </button>
+                <div className="flex gap-2 w-full md:w-auto">
+                  <button
+                    onClick={() => navigate("/prestamos/solicitar")}
+                    className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-2 rounded font-semibold shadow transition flex items-center gap-2"
+                  >
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                    </svg>
+                    Solicitar Préstamo
+                  </button>
+                  <button
+                    onClick={() => navigate("/prestamos/crear")}
+                    className="bg-teal-500 hover:bg-blue-700 text-white px-5 py-2 rounded font-semibold shadow transition flex items-center gap-2"
+                  >
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v7.5m2.25-6.466a9.016 9.016 0 0 0-3.461-.203c-.536.072-.974.478-1.021 1.017a4.559 4.559 0 0 0-.018.402c0 .464.336.844.775.994l2.95 1.012c.44.15.775.53.775.994 0 .136-.006.27-.018.402-.047.539-.485.945-1.021 1.017a9.077 9.077 0 0 1-3.461-.203M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                    Crear nuevo Préstamo
+                  </button>
+                </div>
               </div>
 
               {loading ? (
