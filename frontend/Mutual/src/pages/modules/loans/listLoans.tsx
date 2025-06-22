aba6fbb000426fdcf66fc07afc16824e59e31a2f
Abril-Oli <abril.olivera2003@gmail.com>
2025-06-22 02:07:48 -0300

    Se agregan validaciones de aprobacion y rechazo para prestamos en estado pendiente. Tambien se ajustan las columnas cuotas y tasa de interes (afuera!) del listado de prestamos

--- frontend/Mutual/src/pages/modules/loans/listLoans.tsx
+++ frontend/Mutual/src/pages/modules/loans/listLoans.tsx
@@ -14,140 +14,123 @@ export const initialLoans = [
     interestRate: 10.5,
     loanDate: "2024-01-15T00:00:00Z",
     dueDate: "2025-01-15T00:00:00Z",
-    category: "Ayudas",
     status: "Finalizado",
     active: true,
-    installments: { current: 12, total: 12 },
+    installments: 12,
   },
   {
     id: 2,
     associateDni: "25678901",
     associateLegalName: "María García",
     amount: 22000.0,
-    interestRate: 12.0,
     loanDate: "2024-02-20T00:00:00Z",
     dueDate: "2025-02-20T00:00:00Z",
-    status: "Vigente",
-    category: "Electrodomesticos",
+    status: "Aprobado",
     active: true,
-    installments: { current: 5, total: 6 },
+    installments: 6,
   },
   {
     id: 3,
     associateDni: "35987654",
     associateLegalName: "Carlos López",
     amount: 8000.0,
-    interestRate: 9.0,
+
     loanDate: "2023-11-01T00:00:00Z",
     dueDate: "2024-11-01T00:00:00Z",
     status: "Rechazado",
-    category: "Electrodomesticos",
     active: false,
-    installments: { current: 0, total: 6 },
+    installments: "-" ,
   },
   {
     id: 4,
     associateDni: "40567890",
     associateLegalName: "Ana Rodríguez",
     amount: 5000.0,
-    interestRate: 11.5,
+
     loanDate: "2024-03-10T00:00:00Z",
     dueDate: "2025-03-10T00:00:00Z",
     status: "Aprobado",
-    category: "Ayudas",
     active: true,
-    installments: { current: 1, total: 3 },
+    installments: 3,
   },
   {
     id: 5,
     associateDni: "33445566",
     associateLegalName: "Pedro Martínez",
     amount: 30000.0,
-    interestRate: 10.0,
+
     loanDate: "2024-04-05T00:00:00Z",
     dueDate: "2025-04-05T00:00:00Z",
     status: "Pendiente",
-    category: "Ayudas",
     active: true,
-    installments: { current: 3, total: 6 },
+    installments: "-",
   },
   {
     id: 6,
     associateDni: "28765432",
     associateLegalName: "Laura Fernández",
     amount: 12000.0,
-    interestRate: 10.8,
+
     loanDate: "2023-10-25T00:00:00Z",
     dueDate: "2024-10-25T00:00:00Z",
     status: "Rechazado",
-    category: "Ayudas",
     active: false,
-    installments: { current: 0, total: 0 },
+    installments: "-",
   },
   {
     id: 7,
     associateDni: "37112233",
     associateLegalName: "Miguel Torres",
     amount: 9500.0,
-    interestRate: 9.8,
     loanDate: "2024-05-18T00:00:00Z",
     dueDate: "2025-05-18T00:00:00Z",
     status: "Aprobado",
     active: true,
-    category: "Ayudas",
-    installments: { current: 1, total: 6 },
+    installments:  6 ,
   },
   {
     id: 8,
     associateDni: "31889900",
     associateLegalName: "Sofía Gómez",
     amount: 18000.0,
-    interestRate: 11.0,
     loanDate: "2024-06-01T00:00:00Z",
     dueDate: "2025-06-01T00:00:00Z",
     status: "Pendiente",
     active: true,
-    category: "Electrodomesticos",
-    installments: { current: 0, total: 0},
+    installments:"-",
   },
   {
     id: 9,
     associateDni: "39001122",
     associateLegalName: "Diego Ruiz",
     amount: 7000.0,
-    interestRate: 10.2,
     loanDate: "2024-01-01T00:00:00Z",
     dueDate: "2025-01-01T00:00:00Z",
     status: "Aprobado",
     active: true,
-    category: "Electrodomesticos",
-    installments: { current: 5, total: 12 },
+    installments: 12 ,
   },
   {
     id: 10,
     associateDni: "27654321",
     associateLegalName: "Valeria Díaz",
     amount: 25000.0,
-    interestRate: 12.5,
     loanDate: "2024-02-01T00:00:00Z",
     dueDate: "2025-02-01T00:00:00Z",
     status: "Aprobado",
     active: true,
-    category: "Electrodomesticos",
-    installments: { current: 5, total: 12 },
+    installments:  12 ,
   },
   {
     id: 11,
     associateDni: "34987654",
     associateLegalName: "Francisco Núñez",
     amount: 10000.0,
-    interestRate: 9.5,
     loanDate: "2024-03-01T00:00:00Z",
     dueDate: "2025-03-01T00:00:00Z",
     status: "Pendiente",
     active: true,
-    category: "Ayudas",
-    installments: { current: 0, total: 0 },
+    installments:"-",
   },
 ];
 
@@ -172,7 +155,9 @@ const Loans: React.FC<DashboardProps> = ({
   const [search, setSearch] = useState<string>("");
   const [estadoFiltro, setEstadoFiltro] = useState<string>("Todos");
   const [showModal, setShowModal] = useState(false);
-  const [modalAction, setModalAction] = useState<"aprobar" | "rechazar" | null>(null);
+  const [modalAction, setModalAction] = useState<"aprobar" | "rechazar" | null>(
+    null
+  );
   const [selectedLoan, setSelectedLoan] = useState<any>(null);
   const [modalError, setModalError] = useState<string | null>(null);
   const [page, setPage] = useState<number>(1);
@@ -201,8 +186,7 @@ const Loans: React.FC<DashboardProps> = ({
         (loan) =>
           loan.associateDni.toLowerCase().includes(lowerCaseSearch) ||
           loan.associateLegalName.toLowerCase().includes(lowerCaseSearch) ||
-          loan.amount.toString().includes(lowerCaseSearch) ||
-          loan.category.toLowerCase().includes(lowerCaseSearch)
+          loan.amount.toString().includes(lowerCaseSearch)
       );
     }
 
@@ -301,8 +285,7 @@ const Loans: React.FC<DashboardProps> = ({
                     <option value="Pendiente">Pendiente</option>
                     <option value="Aprobado">Aprobado</option>
                     <option value="Rechazado">Rechazado</option>
-                    <option value="Rechazado">Vigente</option>
-                    <option value="Rechazado">Finalizado</option>
+                    <option value="Finalizado">Finalizado</option>
                   </select>
                 </div>
 
@@ -311,21 +294,7 @@ const Loans: React.FC<DashboardProps> = ({
                     onClick={() => navigate("/prestamos/solicitar")}
                     className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-2 rounded font-semibold shadow transition flex items-center gap-2"
                   >
-                    
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
-                    </svg>
                     Solicitar Préstamo
-                  </button>
-                  <button
-                    onClick={() => navigate("/prestamos/crear")}
-                    className="bg-teal-500 hover:bg-blue-700 text-white px-5 py-2 rounded font-semibold shadow transition flex items-center gap-2"
-                  >
-                    
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v7.5m2.25-6.466a9.016 9.016 0 0 0-3.461-.203c-.536.072-.974.478-1.021 1.017a4.559 4.559 0 0 0-.018.402c0 .464.336.844.775.994l2.95 1.012c.44.15.775.53.775.994 0 .136-.006.27-.018.402-.047.539-.485.945-1.021 1.017a9.077 9.077 0 0 1-3.461-.203M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                    </svg>
-                    Crear nuevo Préstamo
                   </button>
                 </div>
               </div>
@@ -349,18 +318,14 @@ const Loans: React.FC<DashboardProps> = ({
                       <th className="px-4 py-3 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                         Monto
                       </th>
-                      <th className="px-4 py-3 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                        Tasa Interés
-                      </th>
+
                       <th className="px-4 py-3 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                         Estado
                       </th>
                       <th className="px-4 py-3 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                         Fecha emisión Préstamo
                       </th>
-                      <th className="px-4 py-3 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                        Categoría
-                      </th>
+
                       <th className="px-4 py-3 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                         Cuotas
                       </th>
@@ -394,9 +359,7 @@ const Loans: React.FC<DashboardProps> = ({
                           <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
                             ${loan.amount.toFixed(2)}
                           </td>
-                          <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
-                            {loan.interestRate.toFixed(2)}%
-                          </td>
+
                           <td className="px-4 py-4 whitespace-nowrap">
                             <span
                               className={`px-3 py-1 rounded-full text-xs font-semibold ${
@@ -406,8 +369,6 @@ const Loans: React.FC<DashboardProps> = ({
                                   ? "bg-yellow-100 text-yellow-800"
                                   : loan.status === "Rechazado"
                                   ? "bg-red-100 text-red-800"
-                                  : loan.status === "Vigente"
-                                  ? "bg-blue-100 text-blue-800"
                                   : loan.status === "Finalizado"
                                   ? "bg-orange-100 text-orange-800"
                                   : "bg-gray-100 text-gray-800"
@@ -419,28 +380,26 @@ const Loans: React.FC<DashboardProps> = ({
                           <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
                             {new Date(loan.loanDate).toLocaleDateString()}
                           </td>
-                          <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
-                            {loan.category}
-                          </td>
+
                           <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
                             {loan.installments
-                              ? `${loan.installments.current}/${loan.installments.total}`
+                              ? `${loan.installments}`
                               : "-"}
                           </td>
                           <td className="px-4 py-4 text-right whitespace-nowrap text-sm font-medium">
                             <div className="space-x-2 flex justify-end">
                               {/* acciones rechazo y aprobacion */}
                               <button
-                                className="bg-red-500 hover:bg-red-600 text-white px-4 py-1 rounded transition text-xs font-medium"
-                                onClick={() =>
-                                  handleOpenModal(loan, "rechazar")
-                                }
+                                className="bg-red-500 hover:bg-red-600 text-white px-4 py-1 rounded transition text-xs font-medium disabled:opacity-50"
+                                onClick={() => handleOpenModal(loan, "rechazar")}
+                                disabled={loan.status !== "Pendiente"}
                               >
                                 Rechazar
                               </button>
                               <button
-                                className="bg-green-500 hover:bg-green-600 text-white px-4 py-1 rounded transition text-xs font-medium"
+                                className="bg-green-500 hover:bg-green-600 text-white px-4 py-1 rounded transition text-xs font-medium disabled:opacity-50"
                                 onClick={() => handleOpenModal(loan, "aprobar")}
+                                disabled={loan.status !== "Pendiente"}
                               >
                                 Aprobar
                               </button>
