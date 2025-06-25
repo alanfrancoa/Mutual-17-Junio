cb0480086b71378488814c9d25e6ed02ae6211f4
Abril-Oli <abril.olivera2003@gmail.com>
2025-06-25 15:23:44 -0300

    Interfaces de tipos prestamos

---
+++ frontend/Mutual/src/types/ILoanTypes.ts
@@ -0,0 +1,6 @@
+export interface ICreateLoanTypes {
+  code: string;
+  name: string;
+  interestRate: number; 
+  maxAmount: number;
+}
---
+++ frontend/Mutual/src/types/ILoanTypesList.ts
@@ -0,0 +1,8 @@
+export interface ILoanTypesList {
+  id: number; 
+  code: string; 
+  name: string; 
+  interestRate: number;
+  maxAmount: number;
+  active: string;
+}
